bb6c2370392832509dd43dc2c51b34f29b1f099c
MaksimParshin <mak.yp@yandex.ru>
2023-04-10 15:30:36 +0300

    feat added popup with form

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,8 +1,36 @@
 import React from "react";
 
-export default function PopupWithForm() {
+export default function PopupWithForm(props) {
   return (
     <>
+      {/* общий попап */}
+      <div className={`popup popup_name_${props.name}`}>
+        <div className="popup__container popup__container_name_avatar">
+          <button
+            className="popup__close-button"
+            type="button"
+            aria-label="закрыть попап"
+          ></button>
+          <h2 className="popup__title">{props.title}</h2>
+          <form
+            className={`popup__form popup__form_name_${props.name}`}
+            method="post"
+            name="form-popup"
+            noValidate
+          >
+            {props.children}
+
+            <button
+              type="submit"
+              className="popup__submit-button popup__submit-button_state_save"
+              aria-label="создать аватар"
+            >
+              Сохранить
+            </button>
+          </form>
+        </div>
+      </div>
+
       {/* <!-- попап редактирования аватара --> */}
       <div className="popup popup_name_avatar">
         <div className="popup__container popup__container_name_avatar">
