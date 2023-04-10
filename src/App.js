97a7bb1b8322a9e84a14014e55f81160a2cd1eb7
MaksimParshin <mak.yp@yandex.ru>
2023-04-10 14:27:06 +0300

    fix fix type error in jsx app

--- src/App.js
+++ src/App.js
@@ -209,12 +209,12 @@ function App() {
       {/* <!-- шаблон карточки --> */}
       <template className="element-template">
         <div className="element">
-          <button className="element__recicle-bin" type button></button>
+          <button className="element__recicle-bin" type="button" aria-label="удалить карточку"></button>
           <img className="element__image" src="#" alt="" />
           <div className="element__container">
             <h2 className="element__title"></h2>
             <div className="element__like-container">
-              <button className="element__like-button" type="button"></button>
+              <button className="element__like-button" type="button" aria-label="лайк"></button>
               <p className="element__like-counter">0</p>
             </div>
           </div>
