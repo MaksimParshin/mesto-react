7baa3735a7cf1b5d757bb9a91998734a8f30d7a7
MaksimParshin <mak.yp@yandex.ru>
2023-04-25 12:27:04 +0300

    fix fixed app component

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,6 @@ import AddPlacePopup from "./AddPlacePopup";
 function App() {
   const [currentUser, setCurrentUser] = React.useState({});
 
- 
-
   React.useEffect(() => {
     API.getUserInfo()
       .then((data) => {
@@ -56,13 +54,22 @@ function App() {
   }
 
   function handleUpdateUser(data) {
-    API.setUserInfo(data).then((data) => setCurrentUser(data));
+    API.setUserInfo(data)
+      .then((data) => {
+        setCurrentUser(data);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
     closeAllPopups();
   }
 
   function handleUpdateAvatar(data) {
-    API.setAvatar(data).then((data) => setCurrentUser(data));
-    closeAllPopups();
+    API.setAvatar(data)
+      .then((data) => {
+        setCurrentUser(data);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
   }
 
   const [cards, setCards] = React.useState([]);
@@ -82,28 +89,34 @@ function App() {
       API.deleteLike(card._id).then((newCard) => {
         setCards((state) =>
           state.map((c) => (c._id === card._id ? newCard : c))
-        );
+        ).catch((err) => console.log(err));
       });
     } else {
-      API.putLike(card._id).then((newCard) => {
-        setCards((state) =>
-          state.map((c) => (c._id === card._id ? newCard : c))
-        );
-      });
+      API.putLike(card._id)
+        .then((newCard) => {
+          setCards((state) =>
+            state.map((c) => (c._id === card._id ? newCard : c))
+          );
+        })
+        .catch((err) => console.log(err));
     }
   }
 
   function handleCardDelete(card) {
-    API.deleteCard(card._id).then((data) => {
-      setCards((state) => state.filter((c) => c._id !== card._id));
-    });
+    API.deleteCard(card._id)
+      .then((data) => {
+        setCards((state) => state.filter((c) => c._id !== card._id));
+      })
+      .catch((err) => console.log(err));
   }
 
   function handleAddPlaceSubmit(item) {
-    API.createCard(item).then((card) => {
-      setCards([card, ...cards]);
-    });
-    closeAllPopups();
+    API.createCard(item)
+      .then((card) => {
+        setCards([card, ...cards]);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
   }
 
   function closeEsc(event) {
@@ -116,14 +129,13 @@ function App() {
     document.addEventListener("keydown", closeEsc);
     return () => {
       document.removeEventListener("keydown", closeEsc);
-  
     };
   }, []);
 
   return (
+    <CurrentUserContext.Provider value={currentUser}>
     <div className="page">
-      <CurrentUserContext.Provider value={currentUser}>
-        {/* <CardsContext.Provider value={cards}> */}
+     
         <Header />
         <Main
           cards={cards}
@@ -155,11 +167,10 @@ function App() {
         />
 
         <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-        {/* </CardsContext.Provider> */}
-      </CurrentUserContext.Provider>
+    
     </div>
+    </CurrentUserContext.Provider>
   );
 }
 
 export default App;
-
